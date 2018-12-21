b6874b4af8f05154beeb92d79a7794c3a3ac047e
spx0315 <1445024781@qq.com>
2018-12-21 14:06:38 +0800

    更改1

--- src/game/bullbattle/Player.java
+++ src/game/bullbattle/Player.java
@@ -25,10 +25,3 @@ public class Player {
         System.out.println("player" + getId() + "   " + getName());
     }
 }
-        /*if (amount > 8) {
-                System.out.println("不玩了，哪有那么多牌");
-                }
-                else if(amount < 2){
-        System.out.println("一个人玩什么斗牛？");
-        }
-        else */
